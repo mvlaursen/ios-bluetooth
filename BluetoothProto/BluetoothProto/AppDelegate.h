c8c3796eea4c7970d545f477c2015926291f71e7
Mike Laursen <mailbox@laursen.org>
2018-09-19 11:01:21 -0700

    Generated project. Haven't added any Bluetooth code yet.

---
+++ BluetoothProto/BluetoothProto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BluetoothProto
+//
+//  Created by Mike Laursen on 9/19/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BluetoothProto/BluetoothProto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BluetoothProto
+//
+//  Created by Mike Laursen on 9/19/18.
+//  Copyright © 2018 Appamajigger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
