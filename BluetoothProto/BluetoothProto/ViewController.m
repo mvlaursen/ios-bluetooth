34ae3638cd3ac232bf6a029b29f4f557a5c97998
Mike Laursen <mailbox@laursen.org>
2018-09-19 11:32:35 -0700

    Added minimal code to act as a Bluetooth central.

--- BluetoothProto/BluetoothProto/ViewController.m
+++ BluetoothProto/BluetoothProto/ViewController.m
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Appamajigger. All rights reserved.
 //
 
+#import <CoreBluetooth/CoreBluetooth.h>
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,8 +17,58 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // TODO Consider setting the options in this call.
+    CBCentralManager *myCentralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil options:nil];
+    [myCentralManager scanForPeripheralsWithServices:nil options:nil];
 }
 
+
+- (void)centralManagerDidUpdateState:(nonnull CBCentralManager *)central {
+}
+
+//- (void)encodeWithCoder:(nonnull NSCoder *)aCoder {
+//    <#code#>
+//}
+
+//- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
+//    <#code#>
+//}
+
+//- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+//    <#code#>
+//}
+
+//- (CGSize)sizeForChildContentContainer:(nonnull id<UIContentContainer>)container withParentContainerSize:(CGSize)parentSize {
+//    <#code#>
+//}
+
+//- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+//    <#code#>
+//}
+
+//- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+//    <#code#>
+//}
+
+//- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+//    <#code#>
+//}
+
+//- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
+//    <#code#>
+//}
+
+//- (void)setNeedsFocusUpdate {
+//    <#code#>
+//}
+
+//- (BOOL)shouldUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context {
+//    <#code#>
+//}
+
+//- (void)updateFocusIfNeeded {
+//    <#code#>
+//}
 
 @end
